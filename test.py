f1976689d3a99fdc91fe4f54a0c360ad38092039
unknown <02230276.cst@rub.edu.bt>
2024-02-19 15:59:47 +0600

    changes to the file

---
+++ test.py
@@ -0,0 +1,15 @@
+print ('hello world')
+print ("just run")
+grade = int(input('enter mark:'))
+if grade >= 90:
+    print ('grade: A')
+elif grade >= 80:
+    print ('grade: B')
+elif grade >= 70:
+    print ('grade: C')
+elif grade >=60:
+    print ('grade: D')
+elif grade >=40:
+    print ('grade:E')
+else:
+    print(' grade: Fail')
